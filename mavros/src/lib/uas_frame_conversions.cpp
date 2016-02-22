9a456735fd44f0a8020b5eb9e914629d1a767548
Julius_11 <oiluigm@libero.it>
2016-02-22 16:59:46 +0100

    aircraft modificato per fare la trasformazione delle matrici ENU->NED (R*rif*R)

--- mavros/src/lib/uas_frame_conversions.cpp
+++ mavros/src/lib/uas_frame_conversions.cpp
@@ -28,7 +28,9 @@ static const Eigen::Quaterniond NED_ENU_Q = UAS::quaternion_from_rpy(M_PI, 0.0, 
 // Static quaternion needed for rotating between aircraft and base_link frames
 // +PI rotation around X (Forward) axis transforms from Forward, Right, Down (aircraft)
 // Fto Forward, Left, Up (base_link) frames.
-static const Eigen::Quaterniond AIRCRAFT_BASELINK_Q = UAS::quaternion_from_rpy(M_PI, 0.0, 0.0);
+//GM* -> originale:
+//static const Eigen::Quaterniond AIRCRAFT_BASELINK_Q = UAS::quaternion_from_rpy(M_PI, 0.0, 0.0);
+static const Eigen::Quaterniond AIRCRAFT_BASELINK_Q = UAS::quaternion_from_rpy(M_PI, 0.0, M_PI_2);//diventata uguale a NED_ENU
 
 static const Eigen::Affine3d NED_ENU_AFFINE(NED_ENU_Q);
 static const Eigen::Affine3d AIRCRAFT_BASELINK_AFFINE(AIRCRAFT_BASELINK_Q);
